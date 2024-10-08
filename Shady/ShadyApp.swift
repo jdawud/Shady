cb82bf6c91c2645edb2f61f0834b947359caec60
jdawud <jdawud@gmail.com>
2024-10-07 20:46:45 -0600

    Initial Commit

---
+++ Shady/ShadyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShadyApp.swift
+//  Shady
+//
+//  Created by Junaid Dawud on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct ShadyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
